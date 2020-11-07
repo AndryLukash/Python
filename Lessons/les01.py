c235a33e075b77936b62dbc40376416c5b6e6b05
AndryLukash <andrylukash@gmail.com>
2020-11-07 13:15:59 +0300

    start

---
+++ Lessons/les01.py
@@ -0,0 +1,19 @@
+#utf 8
+print ("Project les01 started")
+print ("privet")
+
+
+name = input("Ваше имя: ")
+print (name, "псина")
+
+decision = input ("Го работать? (Y/N)" )
+if decision == 'Y': # decision == 'Y':
+    pass
+elif decision == 'y':
+        print ("Вам водят по губам")
+elif decision == 'N':
+    print ("Вам не водят по губам((((((((((((((")
+elif decision == 'n':
+        print ("Вам не водят по губам((((((((((((((")
+else:
+    print ("че ты пишешь?")
