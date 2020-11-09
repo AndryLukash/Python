3d76b78e65731a6bfb1bf95dcae79cad58532fbf
AndrewL <andrylukash@gmail.com>
2020-11-09 18:14:26 +0300

    Начало 3го задания

--- Lessons/les01.py
+++ Lessons/les01.py
@@ -1,19 +1,31 @@
 #utf 8
-print ("Project les01 started")
-print ("privet")
+
+import os
+
+print("Project les01 started")
+print("privet")
 
 
 name = input("Ваше имя: ")
-print (name, "псина")
+print(name, "псина")
 
-decision = input ("Го работать? (Y/N)" )
-if decision == 'Y': # decision == 'Y':
-    pass
-elif decision == 'y':
-        print ("Вам водят по губам")
-elif decision == 'N':
-    print ("Вам не водят по губам((((((((((((((")
-elif decision == 'n':
-        print ("Вам не водят по губам((((((((((((((")
+answer = input("Го работать? (Y/N)" )
+if answer.lower() == 'y':
+    print("Шо ты хочиш ат миня?9(((99((")
+    print("1 - Вывести список файлов в текущей директории")
+    print("2 - Вывести информацию о системе")
+    #print("3 - Поработать дома")
+    do = int(input("Укажи номер действия: "))
+
+    if do == 1:
+        print(os.listdir())
+    elif do == 2:
+        pass
+    else:
+        pass
+
+
+elif answer.lower() == 'n':
+        print("Ой, всё(((")
 else:
-    print ("че ты пишешь?")
+    print("Че ты пишешь?")
