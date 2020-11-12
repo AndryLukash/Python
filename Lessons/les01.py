e21b25ee5d61a3269155e9990696d468e566990c
AndrewL <andrylukash@gmail.com>
2020-11-12 21:22:02 +0300

    Всё ещё начало 3го урока

--- Lessons/les01.py
+++ Lessons/les01.py
@@ -1,6 +1,8 @@
 #utf 8
-
-import os
+# lesson 3 - type, dir, help, pip, import + os, sys, psutil
+import os # Подключение модуля
+import sys
+import psutil # Подключение стороннего модуля
 
 print("Project les01 started")
 print("privet")
@@ -14,16 +16,18 @@ if answer.lower() == 'y':
     print("Шо ты хочиш ат миня?9(((99((")
     print("1 - Вывести список файлов в текущей директории")
     print("2 - Вывести информацию о системе")
-    #print("3 - Поработать дома")
+    # имя тек дир, ОС, кодировка файловой, логин пользоаптеля, кол-во процессоров
+    print("3 - Вывести список процессов")
     do = int(input("Укажи номер действия: "))
 
     if do == 1:
-        print(os.listdir())
+        print("В директории ", os.getcwd(), " содержатся файлы: ", os.listdir())
     elif do == 2:
         pass
+    elif do == 3:
+        print(psutil.pids())
     else:
         pass
-
 
 elif answer.lower() == 'n':
         print("Ой, всё(((")
